ae12ee8f978421f2cf54e79dc04b9d51347f65c3
Stephen Swensen <stephen.swensen@gmail.com>
2022-05-30 03:30:27 -0400

    playing around with schema ideas

--- sql/001-create-db.sql
+++ sql/001-create-db.sql
@@ -6,5 +6,50 @@ create database DStruct;
 GO
 
 use DStruct;
-select * from sys.tables;
+GO
+
+create schema access;
+GO
+
+create table access.Tenant
+(
+    TenantID int identity (1,1) primary key,
+    [Name] nvarchar(255) not null,
+    unique (Name)
+);
+GO
+
+-- i.e. active api keys
+create table access.TenantKey
+(
+    -- primary keys are automatically indexed and clustered
+    -- (i.e. physical row storage) if no other clustered index
+    -- is given.
+    TenantKeyID int identity (1,1) primary key,
+    TenantID int not null index IX_TenantKey_TenantID nonclustered,
+    -- maybe make this an encrypted column
+    [Key] nvarchar(max) not null,
+    foreign key (TenantID) references access.Tenant(TenantID)
+);
+
+GO
+
+create schema stack;
+GO
+
+-- intentionally denormalized since a parent Stack
+-- table would be low-value book-keeping
+create table stack.Item
+(
+    -- bigint because will have potentially lots-o-records
+    ItemID bigint identity (1,1) primary key,
+    -- use Row Level Security to filter
+    TenantID int not null index IX_Item_TenantID nonclustered,
+    -- a loose identifier for the stack itself
+    StackID nvarchar(255) not null index IX_Item_StackID,
+    -- expecting this to be json, but could be anything perhaps
+    Value nvarchar(max),
+    foreign key (TenantID) references access.Tenant(TenantID)
+);
+
 GO
