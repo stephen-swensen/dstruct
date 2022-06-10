69b55cd84665213b36516bc37a446f31ac1c5644
Stephen Swensen <stephen.swensen@gmail.com>
2022-06-10 14:38:48 -0400

    refactoring

--- sql/run.sh
+++ sql/run.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 
-sqlcmd -S "$DSTRUCT_DB_SERVER" -U "$DSTRUCT_DB_USER" -P "$DSTRUCT_DB_PASSWORD" -i $1
+FILE_ARG=
+if [ $# -eq 1 ]; then 
+    FILE_ARG="-i$1"
+fi
+
+sqlcmd -S "$DSTRUCT_DB_SERVER" -U "$DSTRUCT_DB_USER" -P "$DSTRUCT_DB_PASSWORD" "$FILE_ARG"
+
