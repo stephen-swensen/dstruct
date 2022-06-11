99841638c9abe13f08f8f8eaad5c191dc88156cc
Stephen Swensen <stephen.swensen@gmail.com>
2022-06-10 22:42:33 -0400

    rough in auth test

--- api/Controllers/StatusController.cs
+++ api/Controllers/StatusController.cs
@@ -1,5 +1,6 @@
-using Microsoft.AspNetCore.Authorization;
+using Dapper;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Data.SqlClient;
 
 namespace api.Controllers;
 
@@ -35,11 +36,35 @@ public class StatusController : ControllerBase
     /// Simple auth check. Lets you validate tenant credentials.
     /// </summary>
     /// <returns></returns>
-    [Authorize]
     [HttpGet("status/auth")]
-    public async Task<IActionResult> StatusAuth()
+    public async Task<IActionResult> StatusAuth(
+        [FromQuery] string tenantName,
+        [FromQuery] string tenantKey)
     {
-
-        return new JsonResult(new Object());
+        var server = Environment.GetEnvironmentVariable("DSTRUCT_DB_SERVER");
+        var user = Environment.GetEnvironmentVariable("DSTRUCT_DB_USER");
+        var password = Environment.GetEnvironmentVariable("DSTRUCT_DB_USER");
+        var connStrBuilder = new SqlConnectionStringBuilder
+        {
+            DataSource = server,
+            UserID = user,
+            Password = password,
+            InitialCatalog = "DStruct"
+        };
+        var connStr = connStrBuilder.ToString();
+        using (var conn = new SqlConnection(connStr))
+        {
+            var sql = "exec access.Authenticate @TenantName, @TenantKey";
+            var values = new { TenantName = tenantName, TenantKey = tenantKey };
+            var result = await conn.ExecuteScalarAsync<int>(sql, values);
+            if (result == 1)
+            {
+                return new JsonResult(new Object());
+            }
+            else
+            {
+                return new StatusCodeResult(401);
+            }
+        }
     }
 }
